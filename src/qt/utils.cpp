11e91ddb7efcc2a3a3da3070a1859f4be7f5f890
tuskeNetwork <159142254+tuskeNetwork@users.noreply.github.com>
2024-08-18 11:32:17 +0800

    minor fix

--- src/qt/utils.cpp
+++ src/qt/utils.cpp
@@ -96,7 +96,7 @@ QString xdgMime(){
         "X-GNOME-FullName=Tuske-GUI\n"
         "Comment=Tuske GUI\n"
         "Keywords=Tuske;\n"
-        "Exec=%1 %u\n"
+        "Exec=\"%1\" %u\n"
         "Terminal=false\n"
         "Type=Application\n"
         "Icon=monero\n"
